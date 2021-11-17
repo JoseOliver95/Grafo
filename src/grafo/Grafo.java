3fd36c6953f7cba2728a70da55094e159ce8a624
hp <hp@usuario>
2021-11-17 00:23:17 -0600

    Cambio de una variable

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -1,7 +1,7 @@
 package grafo;
 public class Grafo {
     public static void main(String[] args) {
-        Lienzo a = new Lienzo();
-        a.setVisible(true);
+        Lienzo b = new Lienzo();
+        b.setVisible(true);
     }
 }
